e2f14083bbc3219380705399a99ca1189bb562f8
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-22 00:05:46 +0300

    Является ли число недели - выходным

---
+++ Exam15/Program.cs
@@ -0,0 +1,10 @@
+﻿// На входе число 1-7 с определением дня недели (выходной или нет)
+
+Console.WriteLine("Введите число дня недели от 1 до 7: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A == 6 | A == 7)
+    Console.WriteLine("Выходные ");
+else
+    Console.WriteLine("Рабочий день ");
+
