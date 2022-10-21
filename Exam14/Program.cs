972925f89b0ce8a31196fe156a4985bb2078e424
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-21 20:51:44 +0300

    Кратное 7 и 23

---
+++ Exam14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Принимает на вход число и проверяет на кратность 7 и 23
+int number=161;
+CheckNumber(number);
+
+void CheckNumber(int number1)
+{
+    int result1=number1%7;
+    int result2=number1%23;
+    if(result1==0 & result2==0)
+        Console.WriteLine("кратно");
+    else
+        Console.WriteLine(result1);
+}
+
