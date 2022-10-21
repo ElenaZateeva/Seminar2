7731ea2ef1d3e3e0e6652dc8b12b576fe3ddc881
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-21 18:15:24 +0300

    Наибольшее число из 2хзначного рандомного числа

--- Exam009/Program.cs
+++ Exam009/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывод случайного числа [10,99] и вывод на печать наибольшего. Н., 78-> 8.
+ 
+int randomNumber = GetRandomNumer(100, 1000);  // от 10 до 99
+Console.Write($"Рандомное число - {randomNumber}");
+int maxValue = FindMaxValue(151);
+Console.WriteLine(maxValue);
+
+int FindMaxValue(int numberA);
+{
+    int max = numberA % 10;
+    numberA = numberA / 10;
+
+    while (numberA > 0)
+    {
+        int lastNumber = numberA % 10;
+        if (lastNumber > max)
+        {
+            max = lastNumber;
+        }
+        numberA = numberA / 10;
+    }
+    return max;
+}
+
+
+// Выдает рандомное число в заданном диапазоне
+int GetRandomNumer(int minValue, int maxValue)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(minValue, maxValue);
+    return randomNumber;
+}
+
+
+
+
